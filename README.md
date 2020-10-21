7aa14d458ae495f00eaf2439885c5e3d359446a4
Konstantin <supra9999@mail.ru>
2020-10-21 17:59:41 +1000

    add test, dynamic rule host in http config

--- README.md
+++ README.md
@@ -4,15 +4,14 @@ This ansible role intended for setting on the host Traefik.
 # Using variables
 
 ### Defaults variables
-| **Inventory**                              | **Description**             |
-| ------------------------------------------ | --------------------------- |
+| **Inventory**               | **Description**             |
+| --------------------------- | --------------------------- |
 | `traefik_distr_ver` | The version of Traefik used. (Default: `v2.3.2`) |
 | `traefik_distr_os` | Operating system. (Default: `linux`) |
 | `traefik_distr_arch` | OS architecture. (Default: `amd64`) |
 | `traefik_url_release` | Traefik Archive URL. (Default: `https://github.com/containous/traefik/releases/download/v2.3.2/traefik_v2.3.2_linux_amd64.tar.gz`) |
 | `traefik_path_bin` | Traefik bin location path. (Default: `/usr/bin`) |
 | `traefik_binary` | Full path of Traefik on the system. (Default: `/usr/bin/traefik_v2.3.2`) |
-| `molecule_test_run` | Designed to disable systemd tasks. For testing, the parameter must be set to `true` in the molecule. (Default: `false`) |
 | `traefik_le_caserver` |  Determines which service issues the certificate. For test runs use - `https://acme-staging-v02.api.letsencrypt.org/directory.`. For production - `https://acme-v02.api.letsencrypt.org/directory`. (Default: `https://acme-v02.api.letsencrypt.org/directory`) |
 | `traefik_le_challenge_type` | Different ACME Challenges. It is possible to use `httpChallenge` and `dnsChallenge`. (Default: `httpChallenge`) |
 | `traefik_le_email` | Required parameter to get the certificate Let’s Encrypt. (Default: `NULL`) |
@@ -23,7 +22,9 @@ The `traefik_http_dynamic_config` variable describes the configuration of servic
 Required variables: `name`, `services_url`, `domain`.
 * `name` - The name of your configuration.
 * `services_url` - Service address.
-* `domain` - The domain on which the service will run.
+* `domain` or `` - The domain on which the service will run.
+
+[Rule http. Resource link](https://doc.traefik.io/traefik/routing/routers/#rule)
 
 TLS Certificates:
 * You can connect your TLS Certificate to each service `tls_resolver` or `tls`.
@@ -34,18 +35,28 @@ Required parameters: `main` and `sans`.
 ```yaml
 # Example:
 traefik_http_dynamic_config:
+  # http, no certificate
   - name: 'name_config__HTTP__'
     services_url: 'http://172.16.1.10:9000'
     domain: 'test-http.example.com'
+  # https with Let`s Encrypt cert + traefik rule custom host
   - name: 'name_config__HTTPS__1'
     services_url: 'http://172.16.1.10:9000'
-    domain: 'subdomain.example.com'
+    domain: 'testdomain.example.com'
     tls_resolver:
       main: 'testdomain.example.com'
       sans: '*.testdomain.example.com'
+  # https with Let`s Encrypt cert + traefik ruled Host
+  - name: 'name_config__HTTPS__1'
+    services_url: 'http://172.16.1.10:9000'
+    raw_domain: 'testdomain.example.com'
+    tls_resolver:
+      main: 'testdomain.example.com'
+      sans: '*.testdomain.example.com'
+  # https with default cert
   - name: 'name_config__HTTPS__2'
     services_url: 'http://172.16.1.10:9000'
-    domain: 'test.example.com'
+    domain: 'testdomain2.example.com'
     tls: {}
 ```
 
@@ -56,27 +67,16 @@ Required variables: `name`, `services_address`, `port`.
 * `services_address` - Service address.
 * `port` - Listening port for traffic setup.
 
-TLS Certificates:
-Likewise with `traefik_http_dynamic_config` parameters.
 ```yaml
 # Example:
 traefik_tcp_dynamic_config:
   - name: 'test_config__TCP__'
-    services_address: 'http://172.16.1.10:9000'
-    port: 8085
-    tls_resolver:
-      main: 'testdomain.example.com'
-      sans: '*.testdomain.example.com'
-  - name: 'test_config__TCP__'
     services_address: 'git.example.com'
     port: 8086
-    tls: {}
-  - name: 'test_config__TCP2__'
-    services_address: 'http://172.16.1.10:9000'
-    port: 8087
 ```
-#### TLS
+#### Transport Layer Security (TLS)
 ##### TLS Let`s Encrypt
+
 There are variables to use letsencrypt service:
 * `traefik_le_caserver`, `traefik_le_challenge_type` and `traefik_le_email` - see above.
 * `traefik_le_dns_challenge_provider` - required variable, if `traefik_le_challenge_type: "dnsChallenge"`
@@ -87,6 +87,9 @@ traefik_le_challenge_type: "dnsChallenge"
 traefik_le_email: "testemail@example.com"
 traefik_le_dns_challenge_provider: "route53"
 ```
+
+[TLS. Resource link](https://doc.traefik.io/traefik/https/tls/)
+
 * `traefik_tls_provider_environments` - sets an additional environment variable for the provider.
 ```yaml
 # Example:
@@ -95,10 +98,11 @@ traefik_tls_provider_environments:
     value: "default"
 ```
 
-| **Provider**                              | **Environments**            |
-| ----------------------------------------- | --------------------------- |
-| `route53` |  `AWS_PROFILE`, `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`. |
+| **Provider**        | **Environments**            |
+| ------------------- | --------------------------- |
+| `route53` | `AWS_PROFILE`, `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`. |
 
+[Provider. Resource link](https://doc.traefik.io/traefik/https/acme/#providers)
 
 ##### Default not-LE certs
 To use your certificates, set - `traefik_tls_cert` and `traefik_tls_key`.
@@ -112,7 +116,9 @@ traefik_tls_key: |
   -----END RSA PRIVATE KEY-----
 ```
 
-# An example of your inventory
+[Default Certificate. Resource link](https://doc.traefik.io/traefik/https/tls/#default-certificate)
+
+# Example file of inventory
 ```yaml
 all:
   hosts:
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,6 +1,7 @@
 import testinfra
 
 host = testinfra.get_hosts('all')
+# Files to check
 traefik_service_content = '''
 # https://raw.githubusercontent.com/containous/traefik/master/contrib/systemd/traefik.service
 
@@ -12,8 +13,8 @@ AssertPathExists=/etc/traefik/traefik.yaml
 
 [Service]
 Environment="AWS_PROFILE=default"
-Environment="AWS_ACCESS_KEY_ID="
-Environment="AWS_SECRET_ACCESS_KEY="
+Environment="AWS_ACCESS_KEY_ID=token_access"
+Environment="AWS_SECRET_ACCESS_KEY=token_secret"
 Type=notify
 ExecStart=/usr/bin/traefik_v2.3.2
 Restart=always
@@ -23,6 +24,7 @@ WatchdogSec=1s
 WantedBy=multi-user.target
 '''
 global_yaml_content = '''
+---
 http:
     middlewares:
         redirect-http-to-https:
@@ -36,6 +38,7 @@ tls:
                 keyFile: /etc/traefik/cert/default.pem
 '''
 traefik_yaml_content = '''
+---
 entryPoints:
   http:
     address: ":80"
@@ -43,7 +46,7 @@ entryPoints:
     address: ":443"
   traefik:
     address: ":8080"
-  test_config__TCP__:
+  test_1:
     address: ":8090"
 
 providers:
@@ -68,11 +71,189 @@ api:
 log:
   level: DEBUG
 '''
+default_crt = '''
+-----BEGIN CERTIFICATE-----
+MIIEJzCCAw+gAwIBAgIBBDANBgkqhkiG9w0BAQsFADCBmjELMAkGA1UEBhMCUlUx
+GDAWBgNVBAgTD1ByaW1vcnNraXkga3JheTEUMBIGA1UEBxMLVmxhZGl2b3N0b2sx
+FzAVBgNVBAoTDlZTVCBjb25zdWx0aW5nMQ0wCwYDVQQLEwRJbmMuMQwwCgYDVQQD
+EwNWU1QxJTAjBgkqhkiG9w0BCQEWFm1haWxAdnN0Y29uc3VsdGluZy5uZXQwHhcN
+MjAwOTE1MDM1NDAwWhcNMjEwOTE1MDM1NDAwWjBtMQswCQYDVQQGEwJSVTELMAkG
+A1UECBMCMjUxCzAJBgNVBAcTAlZMMQwwCgYDVQQKEwNWU1QxDDAKBgNVBAsTA1ZT
+VDEMMAoGA1UEAxMDVlNUMRowGAYJKoZIhvcNAQkBFgt2c3RAbWFpbC5ydTCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALfx07fKv2Cjmu1xmv4yJTdLPurr
+X1rDNGdT551QX4ZkQHhv1oMI/kezHaNLuPQXnTJIRqGXBHhyvinBdWEuuU3rKMJv
+g5HpbUaMnu7pEHe9fxmtgYv8aFu8hCROefrdQdj/xu953G004E372SG8OuV3s+ca
++dvA4ma6ykFQhPRA8LFpfW5s9qzgVe6AbeFn3xE/k33a7Jhw5sSgS8ahUmjayFDU
+GHiqTbCDTzHhPwNYpsbPhYl+mDPqJs/V9rKZ4tkp5KtB6Zi3rbsFe2HTfbmIBLb8
+CxpJOda+IlUXyWff5X6ffRggwaHQ9MsMTY4EOR9cEEOQqNd87mhPwnt2irkCAwEA
+AaOBozCBoDAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBRZXpUlSMz7Xn7R238YlRlC
+vn6+GTALBgNVHQ8EBAMCBeAwMQYDVR0RBCowKIITdHJhZWZpay5zYW5kYm94Lmxh
+bocErBABeIELdnN0QG1haWwucnUwEQYJYIZIAYb4QgEBBAQDAgZAMB4GCWCGSAGG
++EIBDQQRFg94Y2EgY2VydGlmaWNhdGUwDQYJKoZIhvcNAQELBQADggEBAFMMXAmO
+VceME/Ig6Jo7TTpaAB4gS7lEpGg8mU7iUUCRN/hfGmvdobRP0x9HIR+/Qy7Qoa4x
+UIArJgbZcN4RbHLQBmM23fWNGicLQZxVXcJ0Vp4Qu98aeoSRK3+UbVRs65OX1Akm
+a9/yxhkZfAFcuzK7d+8NxKdTB2LjgEi+P/bdCyGATs/avlTSLO2xrAq3N0gsdPFB
+yYZV2iYMGz9KVJ7cS6/ySQ9DlVRoKMDVhvGtbFozbvD34OsEANRGLFU2SdBw2hIs
+28Uq9OXxx7sQjjX9/cG75nEpstCldXgg0Va24Y7RXNhunbC2qTLnXNHdZn+4Fd9k
+DEkKriv9yWu9fX4=
+-----END CERTIFICATE-----
+'''
+default_pem = '''
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAt/HTt8q/YKOa7XGa/jIlN0s+6utfWsM0Z1PnnVBfhmRAeG/W
+gwj+R7Mdo0u49BedMkhGoZcEeHK+KcF1YS65Tesowm+DkeltRoye7ukQd71/Ga2B
+i/xoW7yEJE55+t1B2P/G73ncbTTgTfvZIbw65Xez5xr528DiZrrKQVCE9EDwsWl9
+bmz2rOBV7oBt4WffET+TfdrsmHDmxKBLxqFSaNrIUNQYeKpNsINPMeE/A1imxs+F
+iX6YM+omz9X2spni2Snkq0HpmLetuwV7YdN9uYgEtvwLGkk51r4iVRfJZ9/lfp99
+GCDBodD0ywxNjgQ5H1wQQ5Co13zuaE/Ce3aKuQIDAQABAoIBAGFxrHTq5SMuyH4M
+9sy+h9SY+gW9FtCgJHywiDxgPZqWNUkYBg14kCwviP3euWYltFLU0wX7WJTuRCse
+ny4ScHwEnZ0TMiH0BL2g9PkfYmfsbQ6PSdd8qVQfs+j/4cKxv5ZTOXHXnpwDrOfj
+CQ3EB3dUSGufcMmnjshZH5gmLaiubxTYTV4X83XHICEEs7YNAgoxZLWU0iSssseo
+VD4pEnxik0gZIzNh2HRpJuCCuk7T9PSxZwUqI8yUM8TcdPFaX1CVxcKTTuPOkLU1
+FzvBm94ecpjoGHhI026dHymVXGTX1nHKeOQay4m+ANutKDeOTBVzqMh8uTDxffcY
++nZNLpkCgYEA8aGKKOcZZBXZcH0f1oNpEUhrNMArUunAjBmCEmKYSEALih7UIlML
+px4ZTZYSSa6vtI/FvXAJFzeUF6FRhotuXl/xud7PbPSpKzNhO/vkYI9om4iuTe4V
+6Bt+vCZKC/obvBZzKPYq5FuJ5p1H8t5UPU3s0INu6BmivY6Mi8YnEO8CgYEAwuIZ
+3GxFH5tFFjJbBrCPiLp0fZmeBgF1tTM2rRSYnZQPO/40M7LglmRj74QcvqpXIRPR
+nGYT8Vc9NRbj7c1dCMeyb24wUbuCUT7or+KSosNgEqmlUEZtMKwCb2FHtz+ESAKw
+iLPusyMjTaOgyMj5tWLrZA65bqU0UfgL90FpztcCgYEA3eygGDEpAOOQceB5/Wt0
+dIsu66QUJnLKheJntLsZYkJwVss5ysy7RyI92ySaZ1Ipzoy98N+Ved1yBsIDPq+R
+DtzQBwa9PDT5qtPl2lHfz0/no/JFJQ4m0KdwHx6Lo7RzBK0Z1+VAP7xTS5vpGQda
+F3QarOgKVOI2tiLWswmrl8ECgYBOoP8iKiTxjaogPIzxdAdVeTVK/9H8RlaBl3o1
+9xtkaLqE0Bmryj5HL5YKq1kVrJCe7eoimiTtuO8fHm80ISmqQzhBMUoZj/gSY7lq
+TZYwSXfXz869Rs0DWENbQPq4es4aZ8tmPILQkBrMVnqmyD/H6XNj2XUDqXAkqlnZ
+lvDj3QKBgQDrbenRM+aZ/gGnmSFX/bEWEMfP9henvza9KZiq3TjzN2YSY+xL1NAx
+DxDbIV+PFMVAX9OynGJuDtc7dwWtVH5YqlCxmb5gRfLiYH1qIj6ek/l1ASVPztLI
+pEsGJQKqucl+D1lho20uWAoCeeXOSTLwpi7TdIwJEtciMMvQtk1LvQ==
+-----END RSA PRIVATE KEY-----
+'''
+test_1_http = '''
+---
+http:
+  routers:
+    http_test_1:
+      rule: Host(`testdomain.example.com`)
+      entrypoints:
+        - http
+      service: test_1
+      middlewares:
+      - redirect-http-to-https
+    https_test_1:
+      rule: Host(`testdomain.example.com`)
+      entrypoints:
+        - https
+      service: test_1
+      tls:
+        certResolver: default_le_resolver
+        domains:
+          - main: testdomain.example.com
+            sans:
+              - '*.testdomain.example.com'
+  services:
+    test_1:
+      loadBalancer:
+        servers:
+          - url: http://172.16.1.10:9000
+'''
+test_2_http = '''
+---
+http:
+  routers:
+    http_test_2:
+      rule: Host(`dom1.example.com`)
+      entrypoints:
+        - http
+      service: test_2
+      middlewares:
+      - redirect-http-to-https
+    https_test_2:
+      rule: Host(`dom1.example.com`)
+      entrypoints:
+        - https
+      service: test_2
+      tls: {}
+
+
+  services:
+    test_2:
+      loadBalancer:
+        servers:
+          - url: http://172.16.1.10:9000
+'''
+test_3_http = '''
+---
+http:
+  routers:
+    http_test_3:
+      rule: Host(`dom2.example.com`)
+      entrypoints:
+        - http
+      service: test_3
+
+  services:
+    test_3:
+      loadBalancer:
+        servers:
+          - url: http://172.16.1.10:9000
+'''
+test_4_http = '''
+---
+http:
+  routers:
+    http_test_4:
+      rule: 'HostRegexp(`testdomain2.example.com`, \
+`{subdomain:[a-zA-Z0-9-]+}.testdomain2.example.com`)'
+      entrypoints:
+        - http
+      service: test_4
+      middlewares:
+      - redirect-http-to-https
+    https_test_4:
+      rule: 'HostRegexp(`testdomain2.example.com`, \
+`{subdomain:[a-zA-Z0-9-]+}.testdomain2.example.com`)'
+      entrypoints:
+        - https
+      service: test_4
+      tls:
+        certResolver: default_le_resolver
+        domains:
+          - main: testdomain2.example.com
+            sans:
+              - '*.testdomain2.example.com'
+  services:
+    test_4:
+      loadBalancer:
+        servers:
+          - url: http://172.16.1.10:9000
+'''
+test_1_tcp = '''
+---
+tcp:
+  routers:
+    tcp_test_1:
+      entryPoints:
+        - "test_1"
+      service: "test_1"
+      rule: "HostSNI(`*`)"
+
+  services:
+    test_1:
+      loadBalancer:
+        servers:
+          - address: git.example.com:22
+'''
 
 check_files = {
     '/etc/systemd/system/traefik.service': traefik_service_content,
+    '/etc/traefik/cert/default.crt': default_crt,
+    '/etc/traefik/cert/default.pem': default_pem,
+    '/etc/traefik/traefik.yaml': traefik_yaml_content,
     '/etc/traefik/dynamic/_global.yaml': global_yaml_content,
-    '/etc/traefik/traefik.yaml': traefik_yaml_content
+    '/etc/traefik/dynamic/test_1_http.yaml': test_1_http,
+    '/etc/traefik/dynamic/test_2_http.yaml': test_2_http,
+    '/etc/traefik/dynamic/test_3_http.yaml': test_3_http,
+    '/etc/traefik/dynamic/test_4_http.yaml': test_4_http,
+    '/etc/traefik/dynamic/test_1_tcp.yaml': test_1_tcp
 }
 
 
